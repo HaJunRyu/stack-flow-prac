475bdc6725144defe9fb27cd10c45431ab260ffd
HaJunRyu <hjr4557@naver.com>
2023-07-14 23:48:23 +0900

    MyActivity와 Article 액티비티를 생성하고 push, pop 동작을 구현한다

--- src/stackflow.ts
+++ src/stackflow.ts
@@ -1,14 +1,20 @@
 import { stackflow } from '@stackflow/react';
 import { basicRendererPlugin } from '@stackflow/plugin-renderer-basic';
 import { basicUIPlugin } from '@stackflow/plugin-basic-ui';
+import MyActivity from './MyActivity';
+import Article from './Article';
 
 export const { Stack, useFlow } = stackflow({
   transitionDuration: 350,
-  activities: {},
   plugins: [
     basicRendererPlugin(),
     basicUIPlugin({
       theme: 'cupertino',
     }),
   ],
+  activities: {
+    MyActivity,
+    Article,
+  },
+  initialActivity: () => 'MyActivity',
 });
