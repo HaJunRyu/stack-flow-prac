d529b44cb1a5544fd59f86459711fbb1d5323800
HaJunRyu <hjr4557@naver.com>
2023-07-15 00:56:01 +0900

    Step을 push, replace, pop처리한다

--- src/Article.tsx
+++ src/Article.tsx
@@ -7,7 +7,7 @@ type ArticleParams = {
 };
 const Article: ActivityComponentType<ArticleParams> = ({ params }) => {
   // 타입 안정성을 위해 현재 액티비티의 이름을 넣어줘요
-  const { stepPush } = useStepFlow('Article');
+  const { stepPush, stepReplace, stepPop } = useStepFlow('Article');
 
   const onNextClick = () => {
     // `stepPush()`을 호출하면 params.title이 변경돼요.
@@ -16,11 +16,25 @@ const Article: ActivityComponentType<ArticleParams> = ({ params }) => {
     });
   };
 
+  const onChangeClick = () => {
+    // `stepReplace()`을 호출하면 params.title이 변경돼요
+    stepReplace({
+      title: 'Next Title',
+    });
+  };
+
+  const onPrevClick = () => {
+    // `stepPop()`을 호출하면 이전 params.title로 돌아가요
+    stepPop();
+  };
+
   return (
     <AppScreen appBar={{ title: 'Article' }}>
       <div>
         <h1>{params.title}</h1>
         <button onClick={onNextClick}>next</button>
+        <button onClick={onChangeClick}>change</button>
+        <button onClick={onPrevClick}>prev</button>
       </div>
     </AppScreen>
   );
