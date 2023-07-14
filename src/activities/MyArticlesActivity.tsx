6a029c86f221419f6325c3b05dd179700b10ead9
HaJunRyu <hjr4557@naver.com>
2023-07-15 04:45:57 +0900

    내 게시글 관리 액티비티에 pop 동작을 추가한다

--- src/activities/MyArticlesActivity.tsx
+++ src/activities/MyArticlesActivity.tsx
@@ -1,14 +1,21 @@
 import { ActivityComponentType } from '@stackflow/react';
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { Box, Button, Container } from '@material-ui/core';
+import { useFlow } from '../stackflow';
 
 const MyArticlesActivity: ActivityComponentType = () => {
+  const { pop } = useFlow();
+
+  const onPopCurrentActivity = () => {
+    pop();
+  };
+
   return (
     <AppScreen appBar={{ title: 'MyArticlesActivity' }}>
       <Container style={{ textAlign: 'center' }}>
         <Box style={{ marginBottom: 10 }}>My article does not exist.</Box>
-        <Button variant="contained" color="primary">
-          favorites articles
+        <Button variant="contained" color="primary" onClick={onPopCurrentActivity}>
+          back
         </Button>
       </Container>
     </AppScreen>
